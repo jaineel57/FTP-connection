4389876b21255b31ee8678ef51e87f7d632e6b2d
jaineel dharia <jaineeldharia123@gmail.com>
2022-08-11 22:41:33 -0400

    Update server.c

--- server/server.c
+++ server/server.c
@@ -1,1 +1,10 @@
-
+#include <stdio.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <limits.h>
+#include <unistd.h>
